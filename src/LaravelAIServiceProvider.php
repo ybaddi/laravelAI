9a87d0eaa471fbd6dcc2b9cbb2af9147f2ad895f
youssef baddi <baddiyoussef85@gmail.com>
2025-03-01 20:31:52 +0000

    init commit of all project

---
+++ src/LaravelAIServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ybaddi\LaravelAI;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Enregistrer les services du package ici
+    }
+
+    public function boot()
+    {
+        // Charger fichiers de routes, migrations, etc.
+    }
+}
+
